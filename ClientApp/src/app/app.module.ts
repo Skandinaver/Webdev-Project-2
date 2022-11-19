a0b1edff04980661b76d00927b60d5f9699d349a
kuhan1209 <73043883+kuhan1209@users.noreply.github.com>
2022-11-19 13:54:53 +0100

    errors

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -32,9 +33,9 @@ import { reportComponent } from './report/report.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: '', component: loginComponent },
-      { path: '', component: mapsComponent },
-      { path: '', component: reportComponent },
+      { path: 'login', component: loginComponent },
+      { path: 'maps', component: mapsComponent },
+      { path: 'report', component: reportComponent },
     ])
   ],
   providers: [],
